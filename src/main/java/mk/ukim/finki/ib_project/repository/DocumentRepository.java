a7e89c2d4814f5eb457ab55d28cf00713ef0f227
angeldamcheski <damcheskiangel911@gmail.com>
2024-02-25 16:18:29 +0100

    Annotation added

--- src/main/java/mk/ukim/finki/ib_project/repository/DocumentRepository.java
+++ src/main/java/mk/ukim/finki/ib_project/repository/DocumentRepository.java
@@ -2,6 +2,8 @@ package mk.ukim.finki.ib_project.repository;
 
 import mk.ukim.finki.ib_project.model.Document;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DocumentRepository extends JpaRepository<Document,Long> {
 }
--- src/main/java/mk/ukim/finki/ib_project/repository/UserRepository.java
+++ src/main/java/mk/ukim/finki/ib_project/repository/UserRepository.java
@@ -2,7 +2,9 @@ package mk.ukim.finki.ib_project.repository;
 
 import mk.ukim.finki.ib_project.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findUserByEmail(String email);
