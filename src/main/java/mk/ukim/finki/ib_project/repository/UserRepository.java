be0a3a9bbfc8af8d75f826ceea77eed9fd44820a
angeldamcheski <damcheskiangel911@gmail.com>
2024-03-04 15:22:31 +0100

    Encryption and decryption implemented

--- src/main/java/mk/ukim/finki/ib_project/repository/UserRepository.java
+++ src/main/java/mk/ukim/finki/ib_project/repository/UserRepository.java
@@ -2,9 +2,11 @@ package mk.ukim.finki.ib_project.repository;
 
 import mk.ukim.finki.ib_project.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findUserByEmail(String email);
